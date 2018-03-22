048234d6f2628f20b665a4061fa4bfa335a9b575
Damon Freeman <damonefreeman@gmail.com>
2018-03-22 14:01:15 -0500

    Added text to explain results

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,19 @@
 var wifeName = "Lang";
 //var counter = 5;
-
+function resetScreen() {
+  yourFeelings();
+}
 function yourFeelings() {
-  
+  document.getElementById("displayWord1").innerHTML = "";
   var loveCount = prompt(wifeName + ", type in a number between 1 - 10 which indicates how much you love me.",);
   var loveWord = prompt(wifeName + ", what word immediately comes to mind when you think of me?",);
-  var lovePhrase = "You love me " + loveCount + " x " + loveWord + " = " + loveCount + loveWord;
+  //var lovePhrase = "You love me " + loveCount + " x " + loveWord + "  //= " + loveCount + loveWord;
 
    for(var i = 1; i <= loveCount; i++) {
-    document.getElementById("displayWord1").innerHTML += "(" + i + ")" + loveWord + " ";
+    if(i % 2 != 0) {
+      document.getElementById("displayWord1").innerHTML += "(" + i + ")" + loveWord + " ";
+      
    }
-    document.getElementById("displayWord2").innerHTML += lovePhrase + " ";
+       document.getElementById("displayWord2").innerHTML = "You entered " + loveCount + " but here are the odd instances of that number";
+  } 
 };
