f44466e9ef62dc85f8d6f0ca367fa00b3778bd0c
Damon Freeman <damonefreeman@gmail.com>
2018-03-22 12:42:32 -0500

    Added the index() of each string

--- scripts/app.js
+++ scripts/app.js
@@ -5,10 +5,10 @@ function yourFeelings() {
   
   var loveCount = prompt(wifeName + ", type in a number between 1 - 10 which indicates how much you love me.",);
   var loveWord = prompt(wifeName + ", what word immediately comes to mind when you think of me?",);
-  var lovePhrase = "You love me " + loveCount + " x " + loveWord + " = " + loveWord + loveCount;
+  var lovePhrase = "You love me " + loveCount + " x " + loveWord + " = " + loveCount + loveWord;
 
    for(var i = 1; i <= loveCount; i++) {
-    document.getElementById("displayWord1").innerHTML += loveWord + " ";
+    document.getElementById("displayWord1").innerHTML += "(" + i + ")" + loveWord + " ";
    }
     document.getElementById("displayWord2").innerHTML += lovePhrase + " ";
 };
